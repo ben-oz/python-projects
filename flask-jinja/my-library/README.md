96e361cee5cdd85928f01f311862438e934bbe69
ben-oz <ben.oz@outlook.com>
2021-10-24 23:16:04 +0200

    put an SQLite DB

--- flask-jinja/my-library/README.md
+++ flask-jinja/my-library/README.md
@@ -3,7 +3,17 @@
 This is a small project containing basic html and python code with flask and jinja. 
 The website allows to enter book names, authors and ratings to be saved . Finally a list of all books entered will be shown as a list in the home page.
 
-### To be added:
+(I am not interested in improving my front-end web development skills. Just trying to use a database in python code. So, its does not look nice.  )
 
-- make the dictionary an SQLite DB
-- Dockerize
+## Ideas to be realised:
+
+- Make the dictionary an SQLite DB
+- Dockerize the code
+- Use plain SQL alchemy
+- Add a delete record button
+- Add an edit record button
+- ? maybe a button to show the book list in a specific order
+- or maybe a button for filtering the list
+    - which one is easier to do that? plain sqlalchemy or flask-sqlalchemy?
+- Work on rating
+- Add a little bir styling
--- flask-jinja/my-library/main.py
+++ flask-jinja/my-library/main.py
@@ -1,29 +1,46 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-all_books = []
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///my_library.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+# Creating Table
+class Book(db.Model):
+    __tablename__ = 'books'       # normally class name is the table name but we override it
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+
+db.create_all()
 
 
 @app.route('/')
 def home():
-    return render_template('index.html', arg_books=all_books)
+    all_books = db.session.query(Book).all()
+    count = len(all_books)
+    return render_template('index.html', arg_count=count, arg_books=all_books)
+    # query.all returns a list of objects. iterate it in index.html
 
 
-@app.route("/add", methods=["GET", "POST"])
-def add():
+@app.route("/add_book", methods=["GET", "POST"])
+def add_book():
     if request.method == "POST":
-        added_book = {
-            "title": request.form['book_name'],
-            "author": request.form['author_name'],
-            "rating": request.form['book_rating']
-        }
-        all_books.append(added_book)
+        added_book = Book(
+            title = request.form['book_name'],
+            author = request.form['author_name'],
+            rating = request.form['book_rating']
+        )
+        db.session.add(added_book)
+        db.session.commit()
+
         return redirect(url_for('home'))  # it will redirect to home page after adding the new book
 
-    return render_template('add.html')
+    return render_template('add_book.html')
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
+    app.run(host='0.0.0.0', port=80)
